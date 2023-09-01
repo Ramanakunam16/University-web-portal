d2f862492f2805d5ddeebeb78ad3a9f707b061ff
ramanakunam16 <ramanakunam16@gmail.com>
2023-09-01 20:00:38 +0530

    added some restriction

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // importing
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 8007;
-const bodyParser = require("body-parser");
-const lodash = require("lodash");
-const { exec } = require("child_process");
-const fs = require("fs");
-const fileUpload = require("express-fileupload");
-const bcrypt = require("bcrypt");
+const bodyParser = require('body-parser');
+const lodash = require('lodash');
+const { exec } = require('child_process');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+const bcrypt = require('bcrypt');
 
 // import express from "express";
 
@@ -20,16 +20,17 @@ const bcrypt = require("bcrypt");
 // import fs from "fs";
 
 // express middleware
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-const node_xlsx = require("node-xlsx").default;
+const node_xlsx = require('node-xlsx').default;
 app.use(fileUpload());
+app.use(express.json());
 
 // sqlCredentials importing
 
-const mysql = require("mysql");
+const mysql = require('mysql');
 // import sqlDbCredentialsjson from "./db/sqlDbCredentials.json";
-const sqlDbCredentials = require("./db/sqlDbCredentials.json");
+const sqlDbCredentials = require('./db/sqlDbCredentials.json');
 
 // const fileUpload = require("express-fileupload");
 
@@ -54,7 +55,7 @@ function createDbConnection() {
 
 // app.get("/hello", (req, res) => res.send("hello world"));
 
-app.post("/addResults", (req, res) => {
+app.post('/addResults', (req, res) => {
   const body = req.body;
   // body = {
   //   RegistrationNumber: 1,
@@ -68,8 +69,8 @@ app.post("/addResults", (req, res) => {
   //   cs5: 9,
   // };
   var student_marks_details = [];
-  Object.keys(body).forEach((key) => {
-    if (key.slice(0, 2) == "cs") {
+  Object.keys(body).forEach(key => {
+    if (key.slice(0, 2) == 'cs') {
       student_marks_details.push([
         body.RegistrationNumber,
         body.semNo,
@@ -88,22 +89,22 @@ app.post("/addResults", (req, res) => {
 
   // excute query
   mysqlConnection.query(
-    "INSERT INTO studentDetails values(?,?,?,?)",
+    'INSERT INTO studentDetails values(?,?,?,?)',
     [body.RegistrationNumber, body.StudentName, body.semNo, body.sgpa],
     (err, results) => {
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
         console.log(results);
       }
     }
   );
   mysqlConnection.query(
-    "INSERT INTO studentMarksDetails values ? ",
+    'INSERT INTO studentMarksDetails values ? ',
     [student_marks_details],
     (err, results) => {
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
         console.log(results);
       }
@@ -112,7 +113,7 @@ app.post("/addResults", (req, res) => {
   // End Connection
   mysqlConnection.end();
 });
-app.post("/studentMarksDetails", (req, res) => {
+app.post('/studentMarksDetails', (req, res) => {
   const body = req.body;
   // connect to Db
   //const createDbConnection = require("./db/db.js").creatDbConnection;
@@ -121,13 +122,13 @@ app.post("/studentMarksDetails", (req, res) => {
 
   // excute query
   mysqlConnection.query(
-    "SELECT * FROM studentMarksDetails where sem = ? AND registrationNumber = ?",
+    'SELECT * FROM studentMarksDetails where sem = ? AND registrationNumber = ?',
     [body.sem, body.registrationNumber],
     (err, results) => {
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
-        console.log("smd:", results);
+        console.log('smd:', results);
         res.json(results);
       }
     }
@@ -144,7 +145,7 @@ app.post("/studentMarksDetails", (req, res) => {
   // //   ],
   // });
 });
-app.post("/studentDetails", (req, res) => {
+app.post('/studentDetails', (req, res) => {
   const body = req.body;
   // connect to Db
   //const createDbConnection = require("./db/db.js").creatDbConnection;
@@ -153,13 +154,13 @@ app.post("/studentDetails", (req, res) => {
 
   // excute query
   mysqlConnection.query(
-    "SELECT * FROM studentDetails where sem = ? AND registrationNumber = ?",
+    'SELECT * FROM studentDetails where sem = ? AND registrationNumber = ?',
     [body.sem, body.registrationNumber],
     (err, results) => {
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
-        console.log("sd:", results);
+        console.log('sd:', results);
         res.json(results);
       }
     }
@@ -174,7 +175,7 @@ app.post("/studentDetails", (req, res) => {
   // });
 });
 
-app.post("/resultsFileUploadData", (req, res) => {
+app.post('/resultsFileUploadData', (req, res) => {
   // Read excel file
   const workBook = node_xlsx.parse(req.files.resultsFileUpload.data);
   const sheetData = workBook[0].data;
@@ -199,14 +200,14 @@ app.post("/resultsFileUploadData", (req, res) => {
   }
   //console.log(sheetData[0]);
   console.log(studentMarksDetails[0]);
-  res.send("success");
+  res.send('success');
 
   var studentResults = [];
   var studentDetails = [];
 
   for (let i = 0; i < studentMarksDetails.length; i++) {
-    Object.keys(studentMarksDetails[i]).forEach((key) => {
-      if (key.slice(0, 2) == "cs") {
+    Object.keys(studentMarksDetails[i]).forEach(key => {
+      if (key.slice(0, 2) == 'cs') {
         studentResults.push([
           studentMarksDetails[i].registrationNumber,
           studentMarksDetails[i].sem,
@@ -234,22 +235,22 @@ app.post("/resultsFileUploadData", (req, res) => {
   // excute query
 
   mysqlConnection.query(
-    "INSERT INTO studentDetails values ?",
+    'INSERT INTO studentDetails values ?',
     [studentDetails],
     (err, results) => {
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
         console.log(results);
       }
     }
   );
   mysqlConnection.query(
-    "INSERT INTO studentMarksDetails values ? ",
+    'INSERT INTO studentMarksDetails values ? ',
     [studentResults],
     (err, results) => {
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
         console.log(results);
       }
@@ -259,9 +260,9 @@ app.post("/resultsFileUploadData", (req, res) => {
   mysqlConnection.end();
 });
 
-app.post("/convert", (req, res) => {
+app.post('/convert', (req, res) => {
   if (!req.files) {
-    res.status(400).send("No file uploaded");
+    res.status(400).send('No file uploaded');
     return;
   }
 
@@ -273,7 +274,7 @@ app.post("/convert", (req, res) => {
   const mdbFileData = mdbFile.data;
 
   console.log(req.files.file);
-  const nonNullMdbFileData = mdbFileData.filter((byte) => byte !== 0x00);
+  const nonNullMdbFileData = mdbFileData.filter(byte => byte !== 0x00);
   fs.writeFileSync(mdbFile.name, nonNullMdbFileData);
   console.log(mdbFile.name);
 
@@ -282,38 +283,38 @@ app.post("/convert", (req, res) => {
   // console.log(nonNullMdbFileData);
 
   const csvFilePath = `${mdbFile.name}.csv`;
-  const tableName = "excel_data";
+  const tableName = 'excel_data';
 
   // Convert MDB to CSV
   const mdbToCsvCommand = `mdb-export "${mdbFile.name}" "${tableName}" > "${csvFilePath}"`;
-  console.log("Executing command:", mdbToCsvCommand);
+  console.log('Executing command:', mdbToCsvCommand);
   exec(mdbToCsvCommand, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error extracting data: ${error.message}`);
-      res.status(500).send("Error extracting data");
+      res.status(500).send('Error extracting data');
       return;
     }
-    console.log("Data extracted successfully");
+    console.log('Data extracted successfully');
     // Convert CSV to Excel
-    const csvData = fs.readFileSync(csvFilePath, "utf-8");
-    const csvRows = csvData.split("\n");
-    const xlsxData = csvRows.map((row) => [row.split(",")]);
+    const csvData = fs.readFileSync(csvFilePath, 'utf-8');
+    const csvRows = csvData.split('\n');
+    const xlsxData = csvRows.map(row => [row.split(',')]);
     console.log(xlsxData);
-    const xlsxBuffer = xlsx.build([{ name: "Sheet 1", data: xlsxData }]);
+    const xlsxBuffer = xlsx.build([{ name: 'Sheet 1', data: xlsxData }]);
     console.log(xlsxData);
     console.log(xlsxBuffer);
 
     // Send the xlsx data as a response
-    res.setHeader("Content-Disposition", "attachment; filename=output.xlsx");
+    res.setHeader('Content-Disposition', 'attachment; filename=output.xlsx');
     res.type(
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     );
     res.send(xlsxBuffer);
   });
 });
 
 // user account details after account creation
-app.post("/signUp", async (req, res) => {
+app.post('/signUp', async (req, res) => {
   const mysqlConnection = createDbConnection();
   const uname = req.body.uname;
   const email = req.body.email;
@@ -321,65 +322,105 @@ app.post("/signUp", async (req, res) => {
 
   // console.log(body);
 
-  //checking for exiting user and emails
-  try {
-    // Check if username or email already exists
-    const existingUser = await new Promise((resolve, reject) => {
-      mysqlConnection.query(
-        "SELECT user_name, user_email FROM users WHERE user_name = ? OR user_email = ?",
-        [uname, email],
-        (err, results) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(results[0]);
-          console.log("results:", results);
-        }
-      );
-    });
+  // //checking for exiting user and emails
+  // try {
+  //   // Check if username or email already exists
+  //   const existingUser = await new Promise((resolve, reject) => {
+  //     mysqlConnection.query(
+  //       'SELECT user_name, user_email FROM users WHERE user_name = ? OR user_email = ?',
+  //       [uname, email],
+  //       (err, results) => {
+  //         if (err) {
+  //           reject(err);
+  //           return;
+  //         }
+  //         resolve(results[0]);
+  //         console.log('results:', results);
+  //       }
+  //     );
+  //   });
 
-    if (existingUser) {
-      if (existingUser.user_name === uname) {
-        console.log("Username already exists. Please select a new username.");
-      } else if (existingUser.user_email === email) {
-        console.log("Email already exists. Please use a different email.");
+  //   if (existingUser) {
+  //     if (existingUser.user_name === uname) {
+  //       console.log('Username already exists. Please select a new username.');
+  //     } else if (existingUser.user_email === email) {
+  //       console.log('Email already exists. Please use a different email.');
+  //     }
+  //     const message =
+  //       "<script>alert('Username or email already exists.'); window.location.href='signupform.html'</script>";
+  //     res.status(400).send(message);
+  //     return;
+  //   }
+  // console.log(existingUser, existingUser.user_name);
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(passwd, 10);
+
+  console.log(hashedPassword);
+
+  // Insert the user
+  mysqlConnection.query(
+    'INSERT INTO users VALUES (?, ?, ?)',
+    [uname, email, hashedPassword],
+    (err, results) => {
+      if (err) {
+        console.log('Error:', err);
+        res.status(500).send('An error occurred while signing up.');
+        return;
       }
       const message =
-        "<script>alert('Username or email already exists.'); window.location.href='signupform.html'</script>";
-      res.status(400).send(message);
-      return;
+        "<script>alert('Sign up successful.'); window.location.href='signinform.html'</script>";
+      res.send(message);
     }
-    // console.log(existingUser, existingUser.user_name);
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(passwd, 10);
-
-    console.log(hashedPassword);
-
-    // Insert the user
-    mysqlConnection.query(
-      "INSERT INTO users VALUES (?, ?, ?)",
-      [uname, email, hashedPassword],
-      (err, results) => {
-        if (err) {
-          console.log("Error:", err);
-          res.status(500).send("An error occurred while signing up.");
-          return;
-        }
-        const message =
-          "<script>alert('Sign up successful.'); window.location.href='signinform.html'</script>";
-        res.send(message);
-      }
-    );
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("An error occurred while signing up.");
-  }
+  );
+  // } .catch (error) {
+  //   console.error('Error:', error);
+  //   res.status(500).send('An error occurred while signing up.');
+  // }
 
   // excute query
 });
-app.post("/signIn", (req, res) => {
+
+app.post('/check-Availability', (req, res) => {
+  const { userName, email } = req.body;
+
+  console.log({ email, userName });
+
+  const mysqlConnection = createDbConnection();
+
+  mysqlConnection.query(
+    'SELECT COUNT(*) AS userNameCount FROM users WHERE user_name=?',
+    [userName],
+    (err, userNameResults) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error checking email availability');
+        return;
+      }
+
+      mysqlConnection.query(
+        'SELECT COUNT(*) AS emailCount FROM users WHERE user_email= ?',
+        [email],
+        (err, emailResults) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Error checking email availability');
+            return;
+          }
+
+          const emailExists = emailResults[0].emailCount > 0;
+          const userNameExists = userNameResults[0].userNameCount > 0;
+
+          res.json({
+            email: emailExists,
+            userName: userNameExists,
+          });
+        }
+      );
+    }
+  );
+});
+app.post('/signIn', (req, res) => {
   const mysqlConnection = createDbConnection();
   const uname = req.body.uname;
   const password = req.body.passwd;
@@ -388,22 +429,28 @@ app.post("/signIn", (req, res) => {
   // excute query
 
   mysqlConnection.query(
-    "SELECT * FROM users WHERE user_name=?",
+    'SELECT * FROM users WHERE user_name=?',
     [uname],
     async (err, results) => {
-      console.log(results);
+      // console.log(results);
       if (err) {
-        console.log("Invalid query", err);
+        console.log('Invalid query', err);
       } else {
         if (results.length === 0 || results[0].user_name !== uname) {
-          res.redirect("index.html");
+          // res.render("check");
+          res.send('invalid user');
         }
-        if (results[0].hasedPassword === password) {
-          res.send("login successfully");
+        const passwordMatch = await bcrypt.compare(
+          password,
+          results[0].hashedPassword
+        );
+        if (passwordMatch) {
+          res.send('login successfully');
         } else {
-          const message =
-            "<script>alert('invalid password'); window.location.href='signinform.html'</script>";
-          res.send(message);
+          res.send('INVALID PASSWORD RETRY AGAIN!');
+          //   const message =
+          //     "<script>alert('invalid password'); window.location.href='signinform.html'</script>";
+          //   res.send(message);
         }
 
         // console.log(results);
@@ -428,6 +475,6 @@ app.post("/signIn", (req, res) => {
 //   });
 // });
 
-app.listen(port, "localhost", () => {
+app.listen(port, 'localhost', () => {
   console.log(`app listening on port ${port}`);
 });
