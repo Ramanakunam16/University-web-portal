dd535f267515003b6421abe3aa97bce88317265a
ramanakunam16 <ramanakunam16@gmail.com>
2023-09-06 00:42:40 +0530

    some changes made

---
+++ db/sqlDbCredentials.js
@@ -0,0 +1,10 @@
+// require('dotenv').config();
+
+sqlDbCredentials = {
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  database: 'studentDatabase',
+  password: 'prototype',
+};
+module.exports = sqlDbCredentials;
